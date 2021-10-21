22cff74d1189c751e8aba6657628430db2d2e5b9
Louis Ouellet <louis@TheMacSwitch.local>
2021-10-21 14:51:23 -0400

    21.10-dev-16

--- dist/js/script.js
+++ dist/js/script.js
@@ -66,9 +66,9 @@ API.Plugins.my_containers = {
 				},function(result) {
 					var dataset = JSON.parse(result);
 					if(dataset.success != undefined){
-						for(const [key, value] of Object.entries(dataset.output.results)){ API.Helper.set(API.Contents,['data','dom','containers',value.id],value); }
+						for(const [key, value] of Object.entries(dataset.output.dom)){ API.Helper.set(API.Contents,['data','dom','containers',value.id],value); }
 						for(const [key, value] of Object.entries(dataset.output.raw)){ API.Helper.set(API.Contents,['data','raw','containers',value.id],value); }
-						API.Builder.table(card.children('.card-body'), dataset.output.results, {
+						API.Builder.table(card.children('.card-body'), dataset.output.dom, {
 							headers:dataset.output.headers,
 							id:'my_containersIndex',
 							modal:true,
